97ee1c2121ba1ae34b9131fab3f4594833e97e70
Ammar Lakis <ammar.lakis@gmail.com>
2021-03-09 04:59:11 +0100

    feat(webhook): Accept branch and repo parameters

--- webhook/webhook.go
+++ webhook/webhook.go
@@ -1,6 +1,7 @@
 package webhook
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -13,7 +14,25 @@ func StartServer() {
 	serverLogger := log.New(os.Stdout, "http", -1)
 	server := http.NewServeMux()
 	server.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
+		repos, repoProvided := r.URL.Query()["repo"]
+		branches, branchProvided := r.URL.Query()["branch"]
+
+		if !repoProvided {
+			w.WriteHeader(400)
+			w.Write([]byte("Repository url is missing"))
+			return
+		}
+
+		if !branchProvided {
+			w.WriteHeader(400)
+			w.Write([]byte("Branch name is missing"))
+			return
+		}
+
+		repo := repos[0]
+		branch := branches[0]
+
+		w.Write([]byte(fmt.Sprintf("got webhook for %s:%s", repo, branch)))
 	})
 	err := http.ListenAndServe("localhost:8000", server)
 	if err != nil {
